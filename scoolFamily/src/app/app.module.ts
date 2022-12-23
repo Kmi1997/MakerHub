605f09a209fcd5e2b9c710e6742183f9d81818a0
Camille <89855408+Kmi1997@users.noreply.github.com>
2022-12-23 14:08:33 +0100

    #dev navbar ok

--- scoolFamily/src/app/app.module.ts
+++ scoolFamily/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InternshipComponent } from './internships/internship/internship.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InternshipComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
